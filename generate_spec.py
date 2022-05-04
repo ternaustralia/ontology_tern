6bb90364e6972d9eb88eb20df298580d1af944f0
Edmond Chuc <e.chuc@uq.edu.au>
2022-05-04 05:27:13 +0000

    Add logging to generate_spec.py

--- generate_spec.py
+++ generate_spec.py
@@ -1,6 +1,13 @@
+import logging
+
 from src.template_settings import template_settings
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
     for item in template_settings:
+        logger.info("Generating %s... ", item.target_path)
         item.callable(item)
+        logger.info("Done.")
