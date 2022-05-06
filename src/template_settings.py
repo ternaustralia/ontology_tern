9fde1a94c92628a6beea0f7a21b14d8eb78d68e6
Edmond Chuc <e.chuc@uq.edu.au>
2022-05-06 01:05:10 +0000

    Add tern:FeatureOfInterest to spec

--- src/template_settings.py
+++ src/template_settings.py
@@ -117,6 +117,19 @@ template_settings = [
     ),
     ClassTemplateSettings(
         template=class_template,
+        target_path="spec/source/core/class-tern:FeatureOfInterest.adoc",
+        options=ClassTemplateOptions(
+            uri=TERN_SHAPES.FeatureOfInterest,
+            adoc_identifier="class-tern:FeatureOfInterest",
+            curie="tern:FeatureOfInterest",
+            ontology_file="docs/tern.ttl",
+            shapes_file="docs/tern.shapes.ttl",
+        ),
+        callable=create_file,
+        lookups=lookups,
+    ),
+    ClassTemplateSettings(
+        template=class_template,
         target_path="spec/source/core/class-time:Instant.adoc",
         options=ClassTemplateOptions(
             uri=TERN_SHAPES.Instant,
